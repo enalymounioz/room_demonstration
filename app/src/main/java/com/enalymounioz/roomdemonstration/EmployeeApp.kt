49ea2429669a996539399aa1f9c185587fc8db39
Eleni Voreopoulou <evoreopoulou@wings-ict-solution.eu>
2022-12-05 14:20:31 +0200

    Initial commit

---
+++ app/src/main/java/com/enalymounioz/roomdemonstration/EmployeeApp.kt
@@ -0,0 +1,10 @@
+package com.enalymounioz.roomdemonstration
+
+import android.app.Application
+
+class EmployeeApp : Application() {
+
+    val db by lazy {
+        EmployeeDatabase.getInstance(this)
+    }
+}
---
+++ app/src/main/java/com/enalymounioz/roomdemonstration/EmployeeDao.kt
@@ -0,0 +1,29 @@
+package com.enalymounioz.roomdemonstration
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EmployeeDao {
+
+    @Insert
+    suspend fun insert(employeeEntity: EmployeeEntity)
+
+    @Update
+    suspend fun update(employeeEntity: EmployeeEntity)
+
+    @Delete
+    suspend fun delete(employeeEntity: EmployeeEntity)
+
+    @Query("SELECT * FROM 'employee-table'")
+    fun fetchAllEmployees(): Flow<List<EmployeeEntity>>
+
+    @Query("SELECT * FROM 'employee-table' where id=:id")
+    fun fetchAllEmployeesById(id: Int): Flow<EmployeeEntity>
+
+
+}
